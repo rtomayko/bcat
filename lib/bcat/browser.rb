367ff0631d0d067b0b518cd97c2c31dbaac81d77
Ryan Tomayko <rtomayko@gmail.com>
2010-06-23 01:23:05 -0700

    bcat -b <browser> - works the same on different platforms

--- lib/bcat/browser.rb
+++ lib/bcat/browser.rb
@@ -1,24 +1,69 @@
 class Bcat
-  module Browser
-    def browser(url, application=ENV['BCAT_APPLICATION'])
+  class Browser
+    ENVIRONMENT =
+      case `uname`
+      when /Darwin/       ; 'Darwin'
+      when /Linux/, /BSD/ ; 'X11'
+      else                  'X11'
+      end
+
+    # browser name -> command mappings
+    COMMANDS = {
+      'Darwin' => {
+        'default'     => "open",
+        'safari'      => "open -a Safari",
+        'firefox'     => "open -a Firefox",
+        'chrome'      => "open -a Google\\ Chrome",
+        'chromium'    => "open -a Chromium",
+        'opera'       => "open -a Opera",
+        'curl'        => "curl -s"
+      },
+
+      'X11'  => {
+        'default'     => "xdg-open",
+        'firefox'     => "firefox",
+        'chrome'      => "google-chrome",
+        'chromium'    => "chromium",
+        'mozilla'     => "mozilla",
+        'epiphany'    => "epiphany",
+        'curl'        => "curl -s"
+      }
+    }
+
+    # alternative names for browsers
+    ALIASES = {
+      'google-chrome' => 'chrome',
+      'google chrome' => 'chrome',
+      'gnome'         => 'epiphany'
+    }
+
+    def initialize(browser, command=ENV['BCAT_COMMAND'])
+      @browser = browser
+      @command = command
+    end
+
+    def open(url)
       command = browser_command
       fork do
         [$stdin, $stdout].each { |fd| fd.close }
-        ENV['BCAT_ARGS'] = "-a '#{application}'" if !application.to_s.empty?
-        ENV['BCAT_URL'] = url
-        ENV['BCAT_COMMAND'] = command
-        exec "/bin/sh -c \"#{command.gsub(/"/, '\"')}\""
+        exec "#{command} #{shell_quote(url)}"
       end
     end
 
-    def browser_command
-      return ENV['BCAT_COMMAND'] if !ENV['BCAT_COMMAND'].to_s.empty?
+    def command
+      return @command if @command
+      browser_command
+    end
 
-      case `uname`
-      when /Darwin/       ; 'open $BCAT_ARGS "$BCAT_URL"'
-      when /Linux/, /BSD/ ; 'xdg-open $BCAT_ARGS "$BCAT_URL"'
-      else                ; 'xdg-open "$BCAT_URL"'
-      end
+    def browser_command(browser=@browser)
+      browser ||= 'default'
+      browser = browser.downcase
+      browser = ALIASES[browser] || browser
+      COMMANDS[ENVIRONMENT][browser]
+    end
+
+    def shell_quote(argument)
+      arg = argument.to_s.gsub(/([\\'])/) { "\\" + $1 }
     end
   end
 end
---
+++ test/test_bcat_browser.rb
@@ -0,0 +1,5 @@
+require 'contest'
+require 'bcat/browser'
+
+class BrowserTest < Test::Unit::TestCase
+end
